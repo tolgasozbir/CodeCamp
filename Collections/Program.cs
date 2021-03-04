e83f154c85cc2c96967c266f266abfa81eddcd5f
Tolga SÖZBİR <tolgasoz1@gmail.com>
2021-03-05 02:24:42 +0300

    05.03.2021

---
+++ Collections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler1 = new List<string>();
+            isimler1.Add("Tolga");
+            isimler1.Add("Levent");
+            Console.WriteLine(isimler1[0]);
+            Console.WriteLine(isimler1[1]);
+
+            Console.WriteLine("-----------");
+
+            List<string> isimler2 = new List<string> { "Tolga", "Levent" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            isimler2.Add("Bülent");
+            Console.WriteLine(isimler2[2]);
+
+
+            Console.WriteLine("-----------");
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
